c2cb45a5be020db057ca97d39ae9dfe6ab84ac53
jagadeesh-debug <jaggurockz4892@gmail.com>
2025-01-26 23:43:18 +0530

    not yet responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,73 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [tasks, setTasks] = useState<string[]>([]);
+  const addTask = (e: React.FormEvent): void => {
+    e.preventDefault();
+    const a = document.getElementById("tasks") as HTMLInputElement;
+    const taskvalue = a?.value.trim();
+    if (!taskvalue) {
+      console.error("Please enter the task");
+      alert("Task cannot be empty!");
+      return;
+    }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    setTasks((prevTasks) => {
+      const updatedTasks = [...prevTasks, taskvalue];
+      localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+      return updatedTasks;
+    });
+    a.value = "";
+  };
+
+
+  const deleteTask=(index:number)=>{
+    setTasks((prevTasks)=>{
+      const updatedTasks = prevTasks.filter((_,i:number)=>i!==index)
+        localStorage.setItem("tasks",JSON.stringify(updatedTasks));
+        return updatedTasks;
+      
+    });
+    }
+  useEffect(() => {
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    }
+  }, []);
+
+
+  return (
+    <div className="contaier h-screen w-auto border  flex flex-col ">
+      <h1 className="text-3xl font-bold text-center mt-2">ToDo-List</h1>
+      <div className="flex  flex-col  items-center w-full gap-y-6 overflow-scroll h-screen mt-3">
+        <div className="flex w-1/2 flex-col gap-x-6  mt-12">
+          <h2 className="text-xl font-semibold text-center">Add a Task</h2>
+          <form className="flex gap-x-4 justify-center" onSubmit={addTask}>
+            <input type="text" placeholder="Enter a task" className="border border-black w-1/2 rounded-md h-9" id="tasks" />
+            <button className="border border-black w-1/6 rounded-md bg-green-400" >Add</button>
+          </form>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className=" flex flex-col  w-1/2 "> 
+          <h2 className="text-xl font-semibold text-center">Tasks</h2>
+          <ul id="lists" className="list-disc pl-5 ">
+            {tasks.map((task, index) => (
+              <li key={index} className="flex justify-between items-center  border border-black rounded-md m-2 h-1/4 hover:cursor-pointer ">
+                <span className="ml-12 "> {task}</span>
+                <button
+                  onClick={() => deleteTask(index)}
+                  className="mr-4  rounded-md w-1/6 bg-red-300"
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+      </div>
+      </div>
     </div>
   );
 }
